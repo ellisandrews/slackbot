778864f1db03a5923ab1a5ce724df97bb61ba9d5
Ellis Andrews <ellis@nufitmedia.com>
2017-07-09 13:07:46 -0400

    Getting somewhere...

--- music_share_bot.py
+++ music_share_bot.py
@@ -10,51 +10,64 @@ AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "share"
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
-def handle_command(command, channel):
-    """
-    Receives commands directed at the bot and determines if they are valid commands. If so, then acts on the commands.
-    If not, returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Use the *{0}* command with a Spotify link.".format(EXAMPLE_COMMAND)
-
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-
-
-def parse_slack_output(slack_rtm_output):
+def parse_rtm_output(slack_rtm_output):
     """
     The Slack Real Time Messaging API is an events firehose. This parsing function returns None unless a message is
-    directed at the Bot, based on its ID.
+    directed at the Bot, in which case the RTM output is returned (a list).
     """
     output_list = slack_rtm_output
 
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
+                return output
 
-    return None, None
+    return None
+
+
+def is_valid_command(command):
+    """
+    Receives commands directed at the bot and returns True if they are valid commands, False otherwise.
+    """
+    if command.startswith(EXAMPLE_COMMAND):
+        return True
+    else:
+        return False
+
+
+def add_reaction(output):
+    sc.api_call(
+        'reactions.add',
+        name='thumbsup',
+        channel=output['channel'],
+        timestamp=output['ts']
+    )
+
+
+def main():
+    rtm_output = sc.rtm_read()
+    bot_output = parse_rtm_output(rtm_output)
+    # bot_output == None if there aren't messages directed at the bot
+    if bot_output:
+        if is_valid_command(bot_output['text'].split(AT_BOT)[1].strip().lower()):
+            print 'valid bot output!'
+            add_reaction(bot_output)
+        else:
+            response = "Not sure what you mean. Use the *{0}* command with a Spotify link.".format(EXAMPLE_COMMAND)
+            sc.api_call("chat.postMessage", channel=bot_output['channel'], text=response, as_user=True)
 
 
 if __name__ == "__main__":
 
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
 
-    if slack_client.rtm_connect():
+    if sc.rtm_connect():
         print("music_share bot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-
-            if command and channel:
-                handle_command(command, channel)
-
+            main()
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
--- print_bot_id.py
+++ print_bot_id.py
@@ -4,12 +4,12 @@ from slackclient import SlackClient
 
 BOT_NAME = 'music_share'
 
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 if __name__ == "__main__":
 
-    api_call = slack_client.api_call("users.list")
+    api_call = sc.api_call("users.list")
 
     if api_call.get('ok'):
         # retrieve all users so we can find our bot
